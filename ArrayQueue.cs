e00ec5dd6a5589cfd1fc0195602788182636572b
Dev12ka3 <A00264885@cambrian.ca>
2023-10-10 13:33:14 -0400

    Add project files.

---
+++ ArrayQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class ArrayQueue<T>
+{
+    protected T[] a;
+    private int j;
+    private int n;
+
+    public ArrayQueue()
+    {
+        a = new T[1];
+        j = 0;
+        n = 0;
+    }
+
+    public void add(T x)
+    {
+        if (n + 1 > a.Length)
+            resize();
+        a[(j + n) % a.Length] = x;
+        n++;
+    }
+
+    public T remove()
+    {
+        if (n == 0)
+            throw new InvalidOperationException("Queue is empty.");
+        T x = a[j];
+        j = (j + 1) % a.Length;
+        n--;
+        if (a.Length >= 3 * n)
+            resize();
+        return x;
+    }
+
+    private void resize()
+    {
+        T[] b = new T[Math.Max(1, n * 2)];
+        for (int k = 0; k < n; k++)
+            b[k] = a[(j + k) % a.Length];
+        a = b;
+        j = 0;
+    }
+
+    public void PrintQueue()
+    {
+        Console.Write("Queue: [");
+        for (int i = 0; i < n; i++)
+        {
+            int index = (j + i) % a.Length;
+            Console.Write($"{a[index]}");
+            if (i < n - 1)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+
+
+
+
+}
---
+++ ArrayQueueHelpers.cs
@@ -0,0 +1,48 @@
+﻿internal static class ArrayQueueHelpers
+{
+    
+    public static void Main(string[] args)
+    {
+        ArrayQueue<char> q = new ArrayQueue<char>();
+
+        for (int i = 0; i < 8; i++)
+        {
+            q.add((char)('a' + i));
+            q.PrintQueue();
+        }
+
+
+        q.add('a');
+        q.add('a');
+        q.add('a');
+        q.PrintQueue();
+
+        for (int i = 0; i < 8; i++)
+        {
+            q.remove();
+            q.PrintQueue();
+
+           
+        }
+        q.add('b');
+        q.remove();
+        q.add('c');
+        q.remove();
+        q.PrintQueue();
+        q.add('d');
+        q.PrintQueue();
+        q.add('e');
+        q.PrintQueue();
+        q.remove();
+        q.PrintQueue();
+        q.add('f');
+        q.PrintQueue();
+        q.add('g');
+        q.PrintQueue();
+        q.add('h');
+        q.PrintQueue();
+        q.remove();
+        q.PrintQueue();
+
+    } 
+}
